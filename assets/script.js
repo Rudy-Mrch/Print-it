bf013e73a0d22407abfd85a8475345f0350f4c19
Rudy-Mrch <rudy.marache@gmail.com>
2023-06-26 21:24:54 +0200

    Avancement du projet reste bullet point

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,63 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Declaration des variables
+
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const img = document.querySelector('.banner-img');
+const dots = document.querySelector('.dots');
+
+let position = 0
+console.log(position);
+slideActive(position)
+
+
+function slideActive() {
+	img.src = "./assets/images/slideshow/"+ slides[position].image;
+
+}
+
+arrowLeft.addEventListener('click', () => {
+console.log('fleche de gauche', position);
+
+if(position === 0)
+	{
+		position = slides.length-1;
+	}
+	else{
+		position --
+	}
+	slideActive()
+});
+
+arrowRight.addEventListener('click', () => {
+	console.log('fleche de droite', position);
+	if(position === slides.length-1)
+	{
+		position = 0;
+	}
+	else{
+		position ++
+	}
+	slideActive()
+});
+
+
+
+slides.forEach((slide, index) => {
+	const myDot = document.createElement('div')
+	if (index === 0 ) { 
+		myDot.className = 'dot dot_selected'
+	}
+	else{
+		myDot.className = "dot"; 
+	}
+	document.querySelector ('.dots').appendChild(myDot)
+});
+
+
+
+
+
